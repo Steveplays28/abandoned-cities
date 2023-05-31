8aa7185611f8b77384b2447b1d1b74ba9a8976d1
Steveplays28 <djspaargaren@outlook.com>
2023-05-31 23:39:35 +0200

    feat: Add simple repeating skyscrapers

---
+++ src/main/java/io/github/steveplays28/abandonedcities/AbandonedCities.java
@@ -0,0 +1,11 @@
+package io.github.steveplays28.abandonedcities;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AbandonedCities implements ModInitializer {
+	public static final String MOD_ID = "abandoned-cities";
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/io/github/steveplays28/abandonedcities/client/AbandonedCitiesClient.java
@@ -0,0 +1,10 @@
+package io.github.steveplays28.abandonedcities.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbandonedCitiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
